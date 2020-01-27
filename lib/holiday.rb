7057bc5fa00115a5c8585bbacc7a49504abb63a3
Damon R <dmarcr1997@gmail.com>
2020-01-27 18:01:16 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   count = 0
   holiday_hash.each do |seasons|
-    season = seasons.keys
+    
     season.to_s
     season.split
     season.each do |s|
