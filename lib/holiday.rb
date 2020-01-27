ad8c03df362b2cdd1021e7566d7c336eaa1ea62b
Damon R <dmarcr1997@gmail.com>
2020-01-27 18:08:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,16 +66,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  count = 0
-  puts holiday_hash[0].to_s
-  # while count < holiday_hash.count
-    
-  #   season = holiday_hash[count].to_s
-  #   season.split("_")
-  #   season.each do |s|
-  #     s.capitalize
-  #   end
-  #   season.join(" ")
+
+  holiday_hash.each do |season|
+    puts season
     
   
   
