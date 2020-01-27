50abb14ffff07aa53fce1f6f7a84d7bf50cb0490
Damon R <dmarcr1997@gmail.com>
2020-01-27 17:08:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter].do|holidays|
-    holidays << supply
+    holidays >> supply
   end
 end
 
